a9042fa2ef8baf9c93d984a4b3def3f5dcbe6c09
Adrian Duran <duranadria99@gmail.com>
2021-03-25 13:32:32 +0100

    Crear Rutas Dinamicas para las monedas

--- src/components/CoinsCard.vue
+++ src/components/CoinsCard.vue
@@ -1,48 +1,46 @@
 <template>
   <div
     class="cards">
-    <div v-for="(coin,index) of coins"
-    :key="index"
-    class="card-coin">
-      <div class="card-content">
-        <img
-          class="card-content__img"
-          :src=coin.iconUrl
-          alt=""
-        />
-        <span class="card-content__text">{{coin.name}}</span>
-      </div>
-      <div class="card-content">
-        <span class="card-content__text uppercase">{{coin.symbol}}</span>
-      </div>
-      <div class="card-content">
-        <span class="card-content__text uppercase">{{coin.uuid}}</span>
-      </div>
-      <div class="card-content">
-        <span class="card-content__text uppercase">+1,12%</span>
-      </div>
-      <div class="card-content">
-        <button class="card-content__btn">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="icon icon-tabler icon-tabler-star"
-            width="28"
-            height="28"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="#000000"
-            fill="none"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <path
-              d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
-            />
-          </svg>
-        </button>
-      </div>
-    </div>
+    <router-link :to="{name: 'Coin', params: {id : coin.uuid,nameCoin: coin.name}}"  class="card-coin" v-for="coin of coins" :key="coin.uuid">
+        <div class="card-content">
+          <img
+            class="card-content__img"
+            :src=coin.iconUrl
+            alt=""
+          />
+          <span class="card-content__text">{{coin.name}}</span>
+        </div>
+        <div class="card-content">
+          <span class="card-content__text uppercase">{{coin.symbol}}</span>
+        </div>
+        <div class="card-content">
+          <span class="card-content__text uppercase">{{coin.uuid}}</span>
+        </div>
+        <div class="card-content">
+          <span class="card-content__text uppercase">+1,12%</span>
+        </div>
+        <div class="card-content">
+          <button class="card-content__btn">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="icon icon-tabler icon-tabler-star"
+              width="28"
+              height="28"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#000000"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path
+                d="M12 17.75l-6.172 3.245l1.179 -6.873l-5 -4.867l6.9 -1l3.086 -6.253l3.086 6.253l6.9 1l-5 4.867l1.179 6.873z"
+              />
+            </svg>
+          </button>
+        </div>
+    </router-link>
   </div>
 </template>
 
