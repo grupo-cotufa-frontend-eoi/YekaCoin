a65a42e8465310dcc33589ae5915c8ef43488941
Nestor Torres <nestoreljudoka@gmail.com>
2021-04-09 15:38:53 +0100

    Añadiendo text shadow al titulo y cambiando json de noticias

--- src/components/logoLetters/LogoLetters.vue
+++ src/components/logoLetters/LogoLetters.vue
@@ -13,7 +13,8 @@ export default {
 <style lang="postcss">
 @import url("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap");
 .logo-letters {
-  @apply m-2 text-2xl  font-extrabold;
-  font-family: "Press Start 2P", cursive;
+  @apply m-2 text-4xl font-extrabold;
+  text-shadow: 1px 1px 1px #000, 3px 3px 5px blue;
+  font-family: "Press Start 2P";
 }
 </style>
